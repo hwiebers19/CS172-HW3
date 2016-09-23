cae9d987aac7d402767a30eba4e4b5f085a73bbd
Heidi Wiebers <hwiebers19@hotmail.com>
2016-09-23 14:21:06 -0700

    new file

---
+++ CS172-HW3/CS172-HW3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS172-HW3
+//
+//  Created by Heidi Wiebers on 9/23/16.
+//  Copyright © 2016 Heidi Wiebers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
